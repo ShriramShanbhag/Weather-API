abeea0d7e69c1d265803fff6adbe1f4a7b2748b1
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-05-01 12:37:37 +0530

    Added Index to city name for faster lookup

--- src/database/migrations/1776863271408-AddIndexToCityName.ts
+++ src/database/migrations/1776863271408-AddIndexToCityName.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddIndexToCityName1776863271408 implements MigrationInterface {
-    name = 'AddIndexToCityName1776863271408'
+  name = 'AddIndexToCityName1776863271408';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE INDEX "IDX_a0ae8d83b7d32359578c486e7f" ON "cities" ("name") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX "IDX_a0ae8d83b7d32359578c486e7f" ON "cities" ("name") `,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_a0ae8d83b7d32359578c486e7f"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_a0ae8d83b7d32359578c486e7f"`,
+    );
+  }
 }
